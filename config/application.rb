701e3949b2c979e70d67ee7810a2a52bdb6bd2b3
Delian Petrov <petrovd@uci.edu>
2016-01-05 23:05:56 -0800

    moving the shared settings (configuration of action_mailer) in the application.rb from development.rb environment

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'yaml'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,6 +10,26 @@ Bundler.require(*Rails.groups)
 module Delianpetrov
   class Application < Rails::Application
     config.serve_static_files = true
+
+    #uses the local_env.yml file as the ENV file
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               ENV["DOMAIN"],
+      user_name:            ENV["GMAIL_USERNAME"],
+      password:             ENV["GMAIL_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
