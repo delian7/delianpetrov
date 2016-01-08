423c73ca3f8ec5929c49c373495b6d58d0eda5ba
Delian Petrov <petrovd@uci.edu>
2016-01-07 18:38:43 -0800

    switching SMTP to hello@delianpetrov.com

--- config/application.rb
+++ config/application.rb
@@ -19,14 +19,16 @@ module Delianpetrov
       end if File.exists?(env_file)
     end
 
+    # Action Mailer
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               ENV["DOMAIN"],
-      user_name:            ENV["GMAIL_USERNAME"],
-      password:             ENV["GMAIL_PASSWORD"],
-      authentication:       'plain',
+      address:              'smtp.zoho.com',
+      port:                 465,
+      user_name:            ENV["SMTP_USERNAME"],
+      password:             ENV["SMTP_PASSWORD"],
+      authentication:       :login,
+      ssl:                  true,
+      tls:                  true,
       enable_starttls_auto: true
     }
 
