0ee780542f2876a6bbc2c61a34b6798e35516321
Delian Petrov <petrovd@uci.edu>
2016-01-23 15:53:15 -0800

    ability to list nicknames

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
 
-  def create_small_url
+  def create
     full_url = params[:full_url]
     nickname = params[:nickname]
 
@@ -17,13 +17,18 @@ class LinksController < ApplicationController
     end
   end
 
+  def list_nicknames
+    redirect_to root_path, notice:
+          "#{Link.all.collect {|link| link.nickname + " --> " + link.full_url}.join("<br>")}"
+  end
+
   def go_to_url
   nickname = params[:path]
   full_url = Link.find_by_nickname(nickname).try(:full_url)
     if full_url.present?
       redirect_to full_url
     else
-      redirect_to root_path, alert: 'Page Not Found. Redirecting to root page'
+      redirect_to root_path, alert: "Page Not Found. Redirecting to root page"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
-  get "small_url", to: "links#create_small_url"
+  get "small_url", to: "links#create"
+  get "small_url/list", to: "links#list_nicknames"
   post "send_email", to: "visitors#send_email"
   match "*path", to: "links#go_to_url", via: :all
 end
