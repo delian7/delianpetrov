8d386e61d853caf8677163efe3f76f63f570fe59
Delian Petrov <delian7@gmail.com>
2022-01-13 18:58:20 -0800

    Dpetrov/deploy on heroku (#18)
    * configuration
    
    * removing sqlite
    
    * changing the links controller
    
    * deleting the Capistrano crap
    
    * we don"t need capistrano

--- Gemfile
+++ Gemfile
@@ -8,9 +8,6 @@ gem 'rails', '~> 5.2.2'
 gem 'google-cloud-firestore', '~> 0.25.0'
 
 gem 'mimemagic', github: 'mimemagicrb/mimemagic', ref: '01f92d86d15d85cfd0f20dabd025dcbd36a8a60f'
-
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 gem 'bootstrap-sass'
@@ -23,10 +20,6 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
 
-gem 'capistrano', '~> 3.7', '>= 3.7.1'
-gem 'capistrano-rails', '~> 1.2'
-gem 'capistrano-passenger', '~> 0.2.0'
-gem 'capistrano-rvm'
 gem 'listen', '~> 3.1.5'
 
 # Use CoffeeScript for .coffee assets and views
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,18 +1,59 @@
+require 'google/cloud/firestore'
 
-require "google/cloud/firestore"
-
+# Return a Google::Cloud::Datastore::Dataset for the configured dataset.
+# The dataset is used to create, read, update, and delete entity objects.
 class Link
+  include ActiveModel::Validations
+  include ActiveModel::Model
 
   attr_accessor :id, :name, :url
 
-  # Return a Google::Cloud::Datastore::Dataset for the configured dataset.
-  # The dataset is used to create, read, update, and delete entity objects.
-  def initialize options
-    @firestore = Google::Cloud::Firestore.new(
-      project_id: Rails.application.config.
-                        database_configuration[Rails.env]["dataset_id"]
-    )
-    @link_ref = @firestore.doc "links/#{options[:name]}"
+  validates :name, presence: true
+  validates :url, presence: true
+
+  class << self
+    def create(params)
+      Link.new(params).save
+    end
+
+    def all
+      links_ref = firestore.col 'links'
+      links = []
+      links_ref.get do |link|
+        links << Link.new(link.data)
+      end
+      links
+    end
+
+    def find_by_name(name)
+      link_ref = firestore.doc "links/#{name}"
+      snapshot = link_ref.get
+      if snapshot.exists?
+        Link.new snapshot.data
+      else
+        false
+      end
+    end
+
+    def firestore
+      Google::Cloud::Firestore.new(
+        credentials: creds
+      )
+    end
+
+    private
+
+    def creds
+      if ENV['GOOGLE_APPLICATION_CREDENTIALS']
+        JSON.parse(ENV['GOOGLE_APPLICATION_CREDENTIALS'])
+      else
+        ENV['GOOGLE_APPLICATION_CREDENTIAL_FILE']
+      end
+    end
+  end
+
+  def initialize(options)
+    @link_ref = Link.firestore.doc "links/#{options[:name]}"
     @name = options[:name]
     @url = options[:url]
     @data = {
@@ -20,39 +61,6 @@ class Link
       url: @url
     }
   end
-
-  def self.firestore
-    Google::Cloud::Firestore.new(
-      project_id: Rails.application.config.
-                        database_configuration[Rails.env]["dataset_id"]
-    )
-  end
-
-  def self.find_by_name name
-    link_ref = firestore.doc "links/#{name}"
-    snapshot = link_ref.get
-    if snapshot.exists?
-      link = Link.new snapshot.data
-      return link
-    else
-      false
-    end
-  end
-
-  def self.create params
-    Link.new(params).save
-  end
-
-  def self.all
-    links_ref = firestore.col "links"
-    links = []
-    links_ref.get do |link|
-      links << Link.new(link.data)
-    end
-    links
-  end
-
-  include ActiveModel::Model
 
   def save
     if valid?
@@ -62,14 +70,4 @@ class Link
       false
     end
   end
-  # [END save]
-
-  # [START validations]
-  include ActiveModel::Validations
-
-  validates :name, presence: true
-  validates :url, presence: true
-  # [END validations]
-
-##################
 end
