163e6260fa22b1149a09db221136b24a8bb2e18e
Delian Petrov <delian7@gmail.com>
2022-02-06 16:32:47 -0800

    Dpetrov/second migrate links (#25)
    * Configure pg gem and docker database
    
    * renaming the link model to legacy_link
    
    * migrating Link model to use the postgres database.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,32 +1,35 @@
 class LinksController < ApplicationController
-
   def create
     url = params[:url]
     name = params[:name]
 
     if url.present? && name.present?
-      Link.create(url: url, name: name)
+      link = Link.where(name: name).first_or_initialize
+      link.url = url
+      link.save
+
       redirect_to root_path, notice: "TinyURL #{name} created/updated."
     else
-      redirect_to root_path, alert: "url and/or name not supplied, please try again"
+      redirect_to root_path, alert: 'url and/or name not supplied, please try again'
     end
   end
 
   def list
-    json = Link.all.map do |link|
-      {:name => link.name, :url => link.url}
+    json = Link.all.order(:name).map do |link|
+      { name: link.name, url: link.url }
     end
 
-    render :json => JSON.pretty_generate(json)
+    render json: JSON.pretty_generate(json)
   end
 
   def go_to_url
     name = params[:path]
     url = Link.find_by_name(name).try(:url)
-      if url.present?
-        redirect_to url
-      else
-        redirect_to root_path, alert: "Page Not Found. Redirecting to root page"
-      end
+
+    if url.present?
+      redirect_to url
+    else
+      redirect_to root_path, alert: 'Page Not Found. Redirecting to home page'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
-  get 'tiny_url', to: "links#create"
-  get "list", to: "links#list"
-  post "send_email", to: "visitors#send_email"
-  get ':path' => "links#go_to_url", :as => :page, :constraints => lambda{|req| req.path !~ /\.(png|jpg|js|css)$/ }
+  get 'tiny_url', to: 'links#create'
+  get 'list', to: 'links#list'
+  post 'send_email', to: 'visitors#send_email'
+  get ':path' => 'links#go_to_url', :as => :page, :constraints => ->(req) { req.path !~ /\.(png|jpg|js|css)$/ }
 end
